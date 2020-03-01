46ccf426b40a428e69a6c6056570e7a8e38cc679
Melnikov, Stanislav <smelnikov@definiens.com>
2020-03-01 22:03:29 +0100

    SLID-2: py binding for class Slide

--- slideio/pybind/pybind.cpp
+++ slideio/pybind/pybind.cpp
@@ -6,12 +6,18 @@
 #include <pybind11/chrono.h>
 
 namespace py = pybind11;
+using namespace slideio;
 
 PYBIND11_MODULE(slideio, m) {
     m.doc() = "Reading of medical images";
-    m.def("open_slide",&slideio::openSlide,py::arg("file_path"), py::arg("driver_id"));
-    m.def("get_driver_ids", &slideio::getDriverIDs);
-    py::class_<slideio::Slide, std::shared_ptr<slideio::Slide>>(m, "Slide")
-        .def("get_numb_scenes", &slideio::Slide::getNumbScenes)
-        .def("get_file_path", &slideio::Slide::getFilePath);
+    m.def("open_slide",&openSlide,py::arg("file_path"), py::arg("driver_id"));
+    m.def("get_driver_ids", &getDriverIDs);
+    py::class_<slideio::Slide, std::shared_ptr<Slide>>(m, "Slide")
+        .def("get_scene", &Slide::getScene, py::arg("index"))
+        .def_property_readonly("numb_scenes", &Slide::getNumbScenes)
+        .def_property_readonly("file_path", &Slide::getFilePath);
+    py::class_<slideio::Scene, std::shared_ptr<Scene>>(m, "Scene")
+        .def_property_readonly("name",&Scene::getName)
+        .def_property_readonly("file_path", &Scene::getFilePath)
+        .def_property_readonly("rect", &Scene::getRect);
 }
