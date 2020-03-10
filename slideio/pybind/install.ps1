5238d5aae67d5a6a59cea6a4af8c18fe5b083f16
Melnikov, Stanislav <smelnikov@definiens.com>
2020-03-10 21:57:24 +0100

    multi python wheel script for windows

---
+++ slideio/pybind/install.ps1
@@ -0,0 +1,11 @@
+$pythons = @("3.6","3.7","3.8")
+for($p=0; $p -lt $pythons.Length; $p++)
+{
+   $pversion = "python/" +  $pythons[$p] + "@slideio/stable"
+   Write-Output $pversion
+   & conan install $pversion -g json -if conan
+   $json = Get-Content -Raw -Path conan/conanbuildinfo.json | ConvertFrom-Json
+   $python_exe = Join-Path -Path $json.dependencies.bin_paths[0] -ChildPath "python.exe"
+   & $python_exe -m pip install --user --upgrade setuptools wheel
+   & $python_exe setup.py sdist bdist_wheel
+}
