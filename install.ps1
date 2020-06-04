9e88d45bb2d29c569be30ca866c1fdc3c047d7cf
Stanislav Melnikov <stanislav.melnikov@gmail.com>
2020-06-04 17:25:08 +0000

    Ci win

---
+++ install.ps1
@@ -0,0 +1,4 @@
+C:\Users\Stas\anaconda3\shell\condabin\conda-hook.ps1
+conda activate conan
+conan remote add slideio https://api.bintray.com/conan/booritas/slideio
+python install.py -a build -c release
