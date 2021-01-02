b230a9a1119c77ecdc8d8d1ef7a1d03663e0cb92
Stanislav Melnikov <stanislav.melnikov@gmail.com>
2021-01-02 11:16:30 +0100

    upgrade jpegxr codec to remove memory leak

--- src/single_tests/memory_leaks/memory_leaks.cpp
+++ src/single_tests/memory_leaks/memory_leaks.cpp
@@ -2,10 +2,10 @@
 #include "slideio/drivers/czi/cziimagedriver.hpp"
 using namespace slideio;
 
-void main()
+int main()
 {
     CZIImageDriver driver;
-    auto slide = driver.openFile("d:/Temp/30-10-2020_NothingRecognized-15986.czi");
+    auto slide = driver.openFile("/src/images/czi/30-10-2020_NothingRecognized-15986.czi");
     auto scene = slide->getScene(0);
     cv::Rect blockRect = { 0,0,1000,1000 };
     cv::Range zSliceRange = { 0, 3 };
@@ -14,5 +14,6 @@ void main()
     cv::Mat mat;
     scene->read4DBlockChannels(blockRect, channelIndices, zSliceRange, timeFrameRange, mat);
     std::cout << "Done" << std::endl;
+    return 0;
     
 }
