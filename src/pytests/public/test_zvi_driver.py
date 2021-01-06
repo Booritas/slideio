6ea6ce7bcf12432942e46eb5299f23aa8cf8bb47
Stanislav Melnikov <stanislav.melnikov@gmail.com>
2021-01-06 15:48:18 +0100

    add pytest

--- src/pytests/public/test_zvi_driver.py
+++ src/pytests/public/test_zvi_driver.py
@@ -22,6 +22,26 @@ class TestZVI(unittest.TestCase):
         with pytest.raises(RuntimeError):
             slideio.open_slide(image_path, "ZVI")
 
+    def test_readblock_emptyChannelIndices(self):
+        """
+        Read a 3D image block w/o setting of channel indices
+
+        """
+        image_path = get_test_image_path(
+            "zvi",
+            "Zeiss-1-Stacked.zvi"
+            )
+            
+        with slideio.open_slide(image_path, "ZVI") as slide:
+            self.assertTrue(slide is not None)
+            with slide.get_scene(0) as scene:
+                # read channel block
+                raster = scene.read_block(slices=(0,3))
+                shape = raster.shape
+                self.assertEqual(shape[0],3)
+                self.assertEqual(shape[1],1040)
+                self.assertEqual(shape[2],1388)
+                self.assertEqual(shape[3],3)
 
 if __name__ == '__main__':
     unittest.main()
