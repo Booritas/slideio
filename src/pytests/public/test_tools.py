122f76c5092030280feb9e37cfb4848db917785f
Stanislav Melnikov <stanislav.melnikov@gmail.com>
2021-01-09 23:03:58 +0100

    fix pytest

--- src/pytests/public/test_tools.py
+++ src/pytests/public/test_tools.py
@@ -17,7 +17,7 @@ class TestTools(unittest.TestCase):
             "GDAL",
             "img_2448x2448_3x8bit_SRC_RGB_ducks.png"
             )
-        with slideio.open_slide(image_path, "GDAL") as slide:
+        with slideio.open_slide(image_path, "gdal") as slide:
             with slide.get_scene(0) as scene:
                 img1 = scene.read_block()
                 img2 = scene.read_block()
