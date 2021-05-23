f6ce974fe6867d84301082edc59f27c0b3137926
booritas <stanislav.melnikov@gmail.com>
2021-05-23 12:33:03 +0000

    small adjustments

--- search.sh
+++ search.sh
@@ -1,5 +1,5 @@
 for i in $(find ~/.conan/data -name \*.a); do # Not recommended, will break on whitespace
-    if nm -u "$i" | grep 'secure_getenv'
+    if nm -u "$i" | grep 'secure_getenv\|__fdelt_chk'
     then
         echo "$i"
     fi
