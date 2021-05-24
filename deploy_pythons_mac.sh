fbb9197add26a7403822e1aae332d1b688162524
Stanislav Melnikov <s.melnikov@smart-reporting.com>
2021-05-24 19:18:49 +0200

    deploy pythons for mac

---
+++ deploy_pythons_mac.sh
@@ -0,0 +1,9 @@
+set -e
+echo "Recipes repository location:${CONAN_CUSTOM_DIR}"
+pythons=("3.9" "3.8" "3.7" "3.6" "3.5" )
+for version in "${pythons[@]}"
+do
+   pversion="python-${version}"
+   echo $pversion
+   conan create ${CONAN_CUSTOM_DIR}/${pversion} slideio/stable
+done
