bf154d6a908b9ee8dd6fbdf27290dd6afa2c3dac
Stanislav Melnikov <stanislav.melnikov@gmail.com>
2022-09-21 22:07:51 +0200

    test

--- src/single_tests/memory_leaks/memory_leaks.cpp
+++ src/single_tests/memory_leaks/memory_leaks.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include "slideio/drivers/czi/cziimagedriver.hpp"
+#include "slideio/drivers/svs/svsimagedriver.hpp"
 using namespace slideio;
 
 int main()
 {
-    CZIImageDriver driver;
-    auto slide = driver.openFile("/src/images/czi/30-10-2020_NothingRecognized-15986.czi");
+    SVSImageDriver driver;
+    auto slide = driver.openFile("d:/Projects/slideio/images/svs/private/0ad27f9885.svs");
     auto scene = slide->getScene(0);
     cv::Rect blockRect = { 0,0,1000,1000 };
-    cv::Range zSliceRange = { 0, 3 };
+    cv::Range zSliceRange = { 0, 1 };
     cv::Range timeFrameRange = { 0, 1 };
     std::vector<int> channelIndices = { 0 };
     cv::Mat mat;
