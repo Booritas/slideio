a288609914cadb14981f162f610a5d4433163951
Stanislav Melnikov <s.melnikov@smart-reporting.com>
2022-12-12 17:38:29 +0100

    building script update

--- src/py-bind/build_py_dists_linux.sh
+++ src/py-bind/build_py_dists_linux.sh
@@ -3,26 +3,26 @@
 build_wheel()
 {
   echo "Processing python $py ..."
-  #rm -rf ./build
-  #rm -rf ../../build_py
+  rm -rf ./build
+  rm -rf ../../build_py
   $py setup.py sdist bdist_wheel
 }
 
 set -e
 
-#rm -rf ./dist
-#rm -rf ../../build
+rm -rf ./dist
+rm -rf ../../build
 
 echo "Build python wheels"
 
-# for dir in /opt/python/cp*
-# do
-#   export py="$dir/bin/python"
-#   build_wheel
-# done
+for dir in /opt/python/cp*
+ do
+   export py="$dir/bin/python"
+   build_wheel
+ done
 
-export py="/opt/python/cp310-cp310/bin/python"
-build_wheel
+#export py="/opt/python/cp310-cp310/bin/python"
+#build_wheel
 
 echo "updating wheels"
 
--- src/py-bind/setup.py
+++ src/py-bind/setup.py
@@ -193,6 +193,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     install_requires=['numpy'],
     extras_require={},
