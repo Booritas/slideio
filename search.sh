2240a9f9440a9c9b36c344450a6686c66154fa74
Stanislav Melnikov <s.melnikov@smart-reporting.com>
2022-12-15 13:04:54 +0100

    ubuntu compilation

--- search.sh
+++ search.sh
@@ -1,5 +1,5 @@
 for i in $(find ~/.conan/data -name \*.a); do # Not recommended, will break on whitespace
-    if nm -u "$i" | grep 'fcntl64'
+    if nm -u "$i" | grep 'lerc_decode'
     then
         echo "$i"
     fi
