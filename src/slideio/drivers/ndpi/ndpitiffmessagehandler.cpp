4b23041cd586c8693044a3ded0ffcefcd10628c9
Stanislav Melnikov <stanislav.melnikov@gmail.com>
2023-01-01 00:33:29 +0100

    windows logging fix

--- src/slideio/drivers/ndpi/ndpitiffmessagehandler.cpp
+++ src/slideio/drivers/ndpi/ndpitiffmessagehandler.cpp
@@ -8,6 +8,34 @@
 
 using namespace slideio;
 
+#ifdef _MSC_VER
+int vasprintf(char** strp, const char* format, va_list ap)
+{
+    int len = _vscprintf(format, ap);
+    if (len == -1)
+        return -1;
+    char* str = (char*)malloc((size_t)len + 1);
+    if (!str)
+        return -1;
+    int retval = vsnprintf(str, len + 1, format, ap);
+    if (retval == -1) {
+        free(str);
+        return -1;
+    }
+    *strp = str;
+    return retval;
+}
+
+int asprintf(char** strp, const char* format, ...)
+{
+    va_list ap;
+    va_start(ap, format);
+    int retval = vasprintf(strp, format, ap);
+    va_end(ap);
+    return retval;
+}
+#endif
+
 void NDPITIFFWarningHandler(const char *module, const char *fmt, va_list ap) {
     if(fmt && *fmt) {
         char* msg(nullptr);
