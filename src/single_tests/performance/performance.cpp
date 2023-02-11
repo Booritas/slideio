9f3fb59073d286385a6bee2f1cad7975d2929147
Stanislav Melnikov <stanislav.melnikov@gmail.com>
2023-02-11 13:40:51 +0100

    improve performance of DCM driver by slide opening

--- src/single_tests/performance/performance.cpp
+++ src/single_tests/performance/performance.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
-#include "slideio/drivers/svs/svsimagedriver.hpp"
+#include "slideio/drivers/dcm/dcmimagedriver.hpp"
 #include "tests/testlib/testtools.hpp"
 using namespace slideio;
 
 void test()
 {
-    SVSImageDriver driver;
-    const std::string filePath = TestTools::getFullTestImagePath("svs", "private/0A847635-D91B-4C99-9407-A15AE3EED72A.SVS");
+    DCMImageDriver driver;
+    const std::string filePath = TestTools::getFullTestImagePath("dcm", "private/H01EBB49P-24900/H01EBB49P-24900_level-0.dcm");
     auto slide = driver.openFile(filePath.c_str());
     auto scene = slide->getScene(0);
-    cv::Rect blockRect[] = {
-        {1000,1000,256,256},
-        {2000,2000,256,256},
-        {3000,3000,256,256},
-        {4000,4000,256,256},
-        {5000,5000,256,256},
-        {6000,6000,256,256},
-        {7000,7000,256,256},
-    };
-    for(auto rc:blockRect) {
-        cv::Mat mat;
-        scene->readBlock(rc, mat);
-    }
+    auto rect = scene->getRect();
+    cv::Mat mat;
+    scene->readBlock(rect, mat);
+    // cv::Rect blockRect[] = {
+    //     {1000,1000,256,256},
+    //     {2000,2000,256,256},
+    //     {3000,3000,256,256},
+    //     {4000,4000,256,256},
+    //     {5000,5000,256,256},
+    //     {6000,6000,256,256},
+    //     {7000,7000,256,256},
+    // };
+    // for(auto rc:blockRect) {
+    //     cv::Mat mat;
+    //     scene->readBlock(rc, mat);
+    // }
     std::cout << "Done" << std::endl;
 }
 
