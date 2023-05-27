ebcf28c0f192d54c36eb5ceb771c79a8296a1252
Stanislav Melnikov <stanislav.melnikov@gmail.com>
2023-05-27 19:12:18 +0200

    add transformer test project

---
+++ src/tests/transformer/test_colortransformer.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include "tests/testlib/testtools.hpp"
+#include "slideio/converter/converter.hpp"
+#include "slideio/slideio/slideio.hpp"
+#include "slideio/slideio/scene.hpp"
+#include "slideio/base/exceptions.hpp"
+#include <boost/filesystem.hpp>
+
+#include "slideio/converter/converterparameters.hpp"
+#include "slideio/core/cvslide.hpp"
+#include "slideio/imagetools/tempfile.hpp"
+#include "slideio/slideio/imagedrivermanager.hpp"
+
+
+TEST(ColorTransfomation, test)
+{
+}
+
