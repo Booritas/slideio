8f2699e819d6a44914fbfa0eaca8ce13ca9876bf
Stanislav Melnikov <stanislav.melnikov@gmail.com>
2023-05-28 10:38:48 +0200

    tests for large image block

--- src/tests/transformer/test_colortransformer.cpp
+++ src/tests/transformer/test_colortransformer.cpp
@@ -81,3 +81,82 @@ TEST(ColorTransfomation, colorspaces3channels)
     }	
 }
 
+TEST(ColorTransfomation, grayColorLargeImage)
+{
+	std::string path = TestTools::getTestImagePath("svs", "JP2K-33003-1.svs");
+	std::shared_ptr<slideio::Slide> slide = openSlide(path, "AUTO");
+	std::shared_ptr<slideio::Scene> originScene = slide->getScene(0);
+	ColorTransformation transformation;
+	transformation.setColorSpace(ColorSpace::GRAY);
+	std::shared_ptr<slideio::Scene> transformedScene = transformScene(originScene, transformation);
+	ASSERT_EQ(1, transformedScene->getNumChannels());
+	std::tuple<int, int, int, int> rect = originScene->getRect();
+	int width = std::get<2>(rect);
+	int height = std::get<3>(rect);
+	int x = width / 3;
+	int y = height / 3;
+	int blockWidth = width / 3;
+	int blockHeight = height / 3;
+	std::tuple<int,int, int, int> blockRect(x,y,blockWidth, blockHeight);
+	std::vector<unsigned char> buffer(blockWidth * blockHeight * 3);
+	originScene->readBlock(blockRect, buffer.data(), buffer.size());
+	cv::Mat originImage(blockHeight, blockWidth, CV_8UC3, buffer.data());
+	std::vector<uint8_t> transformedBuffer(blockWidth * blockHeight);
+	transformedScene->readBlock(blockRect, transformedBuffer.data(), transformedBuffer.size());
+	cv::Mat transformedImage(blockHeight, blockWidth, CV_8UC1, transformedBuffer.data());
+	cv::Mat originGray;
+	cv::cvtColor(originImage, originGray, cv::COLOR_RGB2GRAY);
+	TestTools::compareRasters(originGray, transformedImage);
+}
+
+TEST(ColorTransfomation, colorspaces3channelsLargeImage)
+{
+	struct ColorSpaceItem
+	{
+		ColorSpace colorSpace;
+		int cvTransformation;
+
+	};
+	ColorSpaceItem items[] = {
+		{ ColorSpace::HSV, cv::COLOR_RGB2HSV},
+		{ ColorSpace::HLS, cv::COLOR_RGB2HLS},
+		{ ColorSpace::YUV, cv::COLOR_RGB2YUV},
+		{ ColorSpace::YCbCr, cv::COLOR_RGB2YCrCb},
+		{ ColorSpace::LUV, cv::COLOR_RGB2Luv},
+		{ ColorSpace::LAB, cv::COLOR_RGB2Lab},
+		{ ColorSpace::XYZ, cv::COLOR_RGB2XYZ} };
+
+
+	std::string path = TestTools::getTestImagePath("svs", "JP2K-33003-1.svs");
+	std::shared_ptr<slideio::Slide> slide = openSlide(path, "AUTO");
+	std::shared_ptr<slideio::Scene> originScene = slide->getScene(0);
+
+	std::tuple<int, int, int, int> rect = originScene->getRect();
+	const int width = std::get<2>(rect);
+	const int height = std::get<3>(rect);
+	const int x = width / 3;
+	const int y = height / 3;
+	const int blockWidth = width / 3;
+	const int blockHeight = height / 3;
+	const std::tuple<int, int, int, int> blockRect(x, y, blockWidth, blockHeight);
+	const int bufferSize = blockWidth * blockHeight * 3;
+	std::vector<unsigned char> buffer(bufferSize);
+	originScene->readBlock(blockRect, buffer.data(), buffer.size());
+	cv::Mat originImage(blockHeight, blockWidth, CV_8UC3, buffer.data());
+	std::vector<uint8_t> transformedBuffer(bufferSize);
+
+	ColorTransformation transformation;
+	for (auto item : items)
+	{
+		ColorSpace colorSpace = item.colorSpace;
+		int cvTransformation = item.cvTransformation;
+		transformation.setColorSpace(colorSpace);
+		std::shared_ptr<slideio::Scene> transformedScene = transformScene(originScene, transformation);
+		ASSERT_EQ(3, transformedScene->getNumChannels());
+		transformedScene->readBlock(blockRect, transformedBuffer.data(), transformedBuffer.size());
+		cv::Mat transformedImage(blockHeight, blockWidth, CV_8UC3, transformedBuffer.data());
+		cv::Mat originGray;
+		cv::cvtColor(originImage, originGray, cvTransformation);
+		TestTools::compareRasters(originGray, transformedImage);
+	}
+}
