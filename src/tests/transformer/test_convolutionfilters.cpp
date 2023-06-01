0efd3d316ed255e73bb0439eb001dad6cb423ece
Stanislav Melnikov <stanislav.melnikov@gmail.com>
2023-06-01 09:28:07 +0200

    finalizing block extension function test

--- src/tests/transformer/test_convolutionfilters.cpp
+++ src/tests/transformer/test_convolutionfilters.cpp
@@ -14,6 +14,81 @@
 
 using namespace slideio;
 
+void testBlockRect(ConvolutionFilterScene& scene, const cv::Rect& sceneRect, int kernelSize)
+{
+    const int width = sceneRect.width;
+    const int height = sceneRect.height;
+
+    // block rect is inside scene rect
+    int x = kernelSize * 3;
+    int y = kernelSize * 3;
+    int blockWidth = width - x*2;
+    int blockHeight = height - y*2;
+    int kernel2 = (kernelSize + 1) / 2;
+    cv::Rect rect(x, y, blockWidth, blockHeight);
+    cv::Rect extendedRect = scene.extendBlockRect(rect);
+    EXPECT_EQ(x - kernel2, extendedRect.x);
+    EXPECT_EQ(y - kernel2, extendedRect.y);
+    EXPECT_EQ(blockWidth + kernel2*2, extendedRect.width);
+    EXPECT_EQ(blockHeight + kernel2*2, extendedRect.height);
+
+    // block rect is equal to scene rect
+    rect = cv::Rect(0, 0, width, height);
+    extendedRect = scene.extendBlockRect(rect);
+    EXPECT_EQ(0, extendedRect.x);
+    EXPECT_EQ(0, extendedRect.y);
+    EXPECT_EQ(width, extendedRect.width);
+    EXPECT_EQ(height, extendedRect.height);
+
+    // block rect is on the top left border
+    blockWidth = width/4;
+    blockHeight = height/4;
+    rect = cv::Rect(0, 0, blockWidth, blockHeight);
+    extendedRect = scene.extendBlockRect(rect);
+    EXPECT_EQ(0, extendedRect.x);
+    EXPECT_EQ(0, extendedRect.y);
+    EXPECT_EQ(blockWidth + kernel2, extendedRect.width);
+    EXPECT_EQ(blockHeight + kernel2, extendedRect.height);
+
+    // block is on the bottom right border
+    x = width/6;
+    y = height/6;
+    blockWidth = width - x;
+    blockHeight = height - y;
+    rect = cv::Rect(x, y, blockWidth, blockHeight);
+    extendedRect = scene.extendBlockRect(rect);
+    EXPECT_EQ(x - kernel2, extendedRect.x);
+    EXPECT_EQ(y - kernel2, extendedRect.y);
+    EXPECT_EQ(blockWidth + kernel2, extendedRect.width);
+    EXPECT_EQ(blockHeight + kernel2, extendedRect.height);
+
+    // block is close to the top left border
+    x = 1;
+    y = 2;
+    blockWidth = width/4;
+    blockHeight = height/4;
+    rect = cv::Rect(x, y, blockWidth, blockHeight);
+    extendedRect = scene.extendBlockRect(rect);
+    EXPECT_EQ(0, extendedRect.x);
+    EXPECT_EQ(0, extendedRect.y);
+    EXPECT_EQ(blockWidth + kernel2 + x, extendedRect.width);
+    EXPECT_EQ(blockHeight + kernel2 + y, extendedRect.height);
+
+    // block is close to the bottom right border
+    const int dx = 1;
+    const int dy = 2;
+    blockWidth = width / 4;
+    blockHeight = height / 4;
+    x = width - blockWidth - dx;
+    y = height - blockHeight - dy;
+    rect = cv::Rect(x, y, blockWidth, blockHeight);
+    extendedRect = scene.extendBlockRect(rect);
+    EXPECT_EQ(x-kernel2, extendedRect.x);
+    EXPECT_EQ(y-kernel2, extendedRect.y);
+    EXPECT_EQ(blockWidth + kernel2 + dx, extendedRect.width);
+    EXPECT_EQ(blockHeight + kernel2 + dy, extendedRect.height);
+}
+
 TEST(ConvolutionFiltes, getBlockExtensionForGaussianBlur)
 {
     std::shared_ptr<TestScene> originScene(new TestScene);
@@ -46,39 +121,30 @@ TEST(ConvolutionFiltes, getBlockExtensionForGaussianBlur)
 TEST(ConvolutionFiltes, extendBlockRect)
 {
     std::shared_ptr<TestScene> originScene(new TestScene);
-    originScene->setRect(cv::Rect(0, 0, 1000, 1000));
-    GaussianBlurFilter filter;
-    filter.setKernelSizeX(3);
-    filter.setKernelSizeY(3);
-    cv::Rect rect(10, 20, 100, 200);
-    ConvolutionFilterScene scene(originScene, filter);
-    cv::Rect extendedRect = scene.extendBlockRect(rect);
-    EXPECT_EQ(8, extendedRect.x);
-    EXPECT_EQ(18, extendedRect.y);
-    EXPECT_EQ(104, extendedRect.width);
-    EXPECT_EQ(204, extendedRect.height);
-    rect = cv::Rect(0, 0, 1000, 1000);
-    extendedRect = scene.extendBlockRect(rect);
-    EXPECT_EQ(0, extendedRect.x);
-    EXPECT_EQ(0, extendedRect.y);
-    EXPECT_EQ(1000, extendedRect.width);
-    EXPECT_EQ(1000, extendedRect.height);
-    rect = cv::Rect(1, 2, 999, 998);
-    extendedRect = scene.extendBlockRect(rect);
-    EXPECT_EQ(0, extendedRect.x);
-    EXPECT_EQ(0, extendedRect.y);
-    EXPECT_EQ(1000, extendedRect.width);
-    EXPECT_EQ(1000, extendedRect.height);
-    rect = cv::Rect(100, 2, 100, 200);
-    extendedRect = scene.extendBlockRect(rect);
-    EXPECT_EQ(98, extendedRect.x);
-    EXPECT_EQ(0, extendedRect.y);
-    EXPECT_EQ(104, extendedRect.width);
-    EXPECT_EQ(204, extendedRect.height);
-    rect = cv::Rect(100, 1, 100, 200);
-    extendedRect = scene.extendBlockRect(rect);
-    EXPECT_EQ(98, extendedRect.x);
-    EXPECT_EQ(0, extendedRect.y);
-    EXPECT_EQ(104, extendedRect.width);
-    EXPECT_EQ(203, extendedRect.height);
+    cv::Rect sceneRect = cv::Rect(0, 0, 1000, 1000);
+    originScene->setRect(sceneRect);
+    {
+        GaussianBlurFilter filter;
+        filter.setKernelSizeX(3);
+        filter.setKernelSizeY(3);
+        ConvolutionFilterScene scene(originScene, filter);
+        testBlockRect(scene, sceneRect, 3);
+    }
+    {
+        MedianBlurFilter filter;
+        filter.setKernelSize(5);
+        ConvolutionFilterScene scene(originScene, filter);
+        testBlockRect(scene, sceneRect, 5);
+    }
+    {
+        SobelFilter filter;
+        filter.setKernelSize(5);
+        ConvolutionFilterScene scene(originScene, filter);
+        testBlockRect(scene, sceneRect, 5);
+    }
+    {
+        ScharrFilter filter;
+        ConvolutionFilterScene scene(originScene, filter);
+        testBlockRect(scene, sceneRect, 3);
+    }
 }
