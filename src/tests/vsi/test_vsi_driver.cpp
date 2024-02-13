9bd7c02c16dfffb3e3749f89e95d351bebc9a9ec
Stanislav Melnikov <stanislav.melnikov@gmail.com>
2024-02-13 17:28:25 +0100

    3d tests

--- src/tests/vsi/test_vsi_driver.cpp
+++ src/tests/vsi/test_vsi_driver.cpp
@@ -3,10 +3,12 @@
 #include <string>
 #include <opencv2/imgproc.hpp>
 
+#include "slideio/core/tools/tools.hpp"
 #include "slideio/drivers/vsi/vsiimagedriver.hpp"
 #include "slideio/drivers/vsi/vsiscene.hpp"
 #include "slideio/drivers/vsi/vsislide.hpp"
 #include "slideio/drivers/vsi/vsifile.hpp"
+#include "slideio/imagetools/cvtools.hpp"
 
 
 namespace slideio
@@ -212,7 +214,7 @@ TEST(VSIImageDriver, VSIFileOpenWithExternalFiles) {
 
 TEST(VSIImageDriver, read3DVolume16bit) {
     std::string filePath = TestTools::getFullTestImagePath("vsi", "vsi-multifile/vsi-ets-test-jpg2k.vsi");
-    std::string testFilePath = TestTools::getFullTestImagePath("vsi", "test-output/vsi-ets-test-jpg2k.tif");
+    std::string testFilePath = TestTools::getFullTestImagePath("vsi", "test-output/vsi-ets-test-jpg2k.vsi - 001 C405, C488 (1, x=0, y=0, w=1645, h=1682).tif");
     slideio::VSIImageDriver driver;
     std::shared_ptr<CVSlide> slide = driver.openFile(filePath);
     ASSERT_TRUE(slide != nullptr);
@@ -233,16 +235,59 @@ TEST(VSIImageDriver, read3DVolume16bit) {
     EXPECT_DOUBLE_EQ(60, scene->getMagnification());
     EXPECT_DOUBLE_EQ(1.E-6, scene->getZSliceResolution());
     EXPECT_EQ(slideio::Compression::Jpeg2000, scene->getCompression());
-    //cv::Rect roi(rect.x + rect.width / 4, rect.y + rect.height / 4, rect.width / 2, rect.height / 2);
-    //cv::Size blockSize(std::lround(roi.width * 0.8), std::lround(roi.height * 0.8));
-    //cv::Mat blockRaster;
-    //scene->readResampledBlock(roi, blockSize, blockRaster);
-    //cv::Mat testRaster;
-    //TestTools::readPNG(testFilePath, testRaster);
-    //cv::Mat testRoi(testRaster, roi);
-    //cv::resize(testRoi, testRoi, blockSize);
-    //TestTools::compareRasters(testRoi, blockRaster);
-    //TestTools::showRasters(testRoi, blockRaster);
+    cv::Rect roi(rect);
+    cv::Size blockSize(roi.width/4, roi.height/4);
+    cv::Mat blockRaster;
+    scene->readResampled4DBlockChannels(roi, blockSize, { 0 }, { 5,6 }, { 0,1 }, blockRaster);
+    cv::Mat testRaster;
+    ImageTools::readGDALImage(testFilePath, testRaster);
+    cv::resize(testRaster, testRaster, blockSize);
+    double similarity = ImageTools::computeSimilarity2(testRaster, blockRaster);
+    EXPECT_GT(similarity, 0.99);
+    //TestTools::showRasters(testRaster, blockRaster);
+}
+
+TEST(VSIImageDriver, read3DStack16bit) {
+    std::string filePath = TestTools::getFullTestImagePath("vsi", "vsi-multifile/vsi-ets-test-jpg2k.vsi");
+    std::string testFilePath5 = TestTools::getFullTestImagePath("vsi", 
+        "test-output/vsi-ets-test-jpg2k.vsi - 001 C405, C488 (1, x=0, y=0, w=1645, h=1682).tif");
+    std::string testFilePath4 = TestTools::getFullTestImagePath("vsi",
+        "test-output/vsi-ets-test-jpg2k.vsi - slice4.(1, x=0, y=0, w=1645, h=1682).tif");
+    slideio::VSIImageDriver driver;
+    std::shared_ptr<CVSlide> slide = driver.openFile(filePath);
+    ASSERT_TRUE(slide != nullptr);
+    const int numScenes = slide->getNumScenes();
+    ASSERT_EQ(1, numScenes);
+    std::shared_ptr<CVScene> scene = slide->getScene(0);
+    const auto rect = scene->getRect();
+    EXPECT_EQ(cv::Rect(0, 0, 1645, 1682), rect);
+    EXPECT_EQ(2, scene->getNumChannels());
+    EXPECT_EQ(DataType::DT_UInt16, scene->getChannelDataType(0));
+    EXPECT_EQ(DataType::DT_UInt16, scene->getChannelDataType(1));
+    EXPECT_EQ(11, scene->getNumZSlices());
+    EXPECT_EQ(1, scene->getNumTFrames());
+    const auto resolution = scene->getResolution();
+    const double val = 0.108333e-6;
+    EXPECT_DOUBLE_EQ(val, resolution.x);
+    EXPECT_DOUBLE_EQ(val, resolution.y);
+    EXPECT_DOUBLE_EQ(60, scene->getMagnification());
+    EXPECT_DOUBLE_EQ(1.E-6, scene->getZSliceResolution());
+    EXPECT_EQ(slideio::Compression::Jpeg2000, scene->getCompression());
+    cv::Rect roi(rect);
+    cv::Size blockSize(roi.width, roi.height);
+    cv::Mat blockRaster;
+    scene->readResampled4DBlockChannels(roi, blockSize, { 0 }, 
+        { 4,6 }, { 0,1 }, blockRaster);
+    EXPECT_EQ(2,blockRaster.size[2]);
+    cv::Mat slice;
+    CVTools::extractSliceFrom3D(blockRaster, 0, slice);
+    cv::Mat testRaster;
+    ImageTools::readGDALImage(testFilePath4, testRaster);
+    double similarity = ImageTools::computeSimilarity2(testRaster, slice);
+    EXPECT_GT(similarity, 0.99);
+    CVTools::extractSliceFrom3D(blockRaster, 1, slice);
+    ImageTools::readGDALImage(testFilePath5, testRaster);
+    EXPECT_GT(similarity, 0.99);
 }
 
 TEST(VSIImageDriver, readMultiscene) {
@@ -276,6 +321,79 @@ TEST(VSIImageDriver, readMultiscene) {
     cv::Mat testRaster;
     TestTools::readPNG(testFilePath, testRaster);
     TestTools::compareRasters(testRaster, blockRaster);
+    //TestTools::showRasters(testRaster, blockRaster);
+}
+
+TEST(VSIImageDriver, readMultisceneResized) {
+    std::string filePath = TestTools::getFullTestImagePath("vsi", "Zenodo/Abdominal/G1M16_ABD_HE_B6.vsi");
+    std::string testFilePath = TestTools::getFullTestImagePath("vsi", "test-output/G1M16_ABD_HE_B6.vsi-40x_01(1,x=5836,y=11793,w=849,h=607).png");
+    slideio::VSIImageDriver driver;
+    std::shared_ptr<CVSlide> slide = driver.openFile(filePath);
+    ASSERT_TRUE(slide != nullptr);
+    const int numScenes = slide->getNumScenes();
+    ASSERT_EQ(3, numScenes);
+    std::shared_ptr<CVScene> scene = slide->getScene(0);
+    const auto rect = scene->getRect();
+    cv::Rect roi(5836, 11793, 849, 607);
+    cv::Size blockSize(roi.width/3, roi.height/3);
+    cv::Mat blockRaster;
+    scene->readResampledBlock(roi, blockSize, blockRaster);
+    cv::Mat testRaster;
+    TestTools::readPNG(testFilePath, testRaster);
+    cv::resize(testRaster, testRaster, blockSize);
+    double similarity = ImageTools::computeSimilarity2(testRaster, blockRaster);
+    ASSERT_GT(similarity, 0.99);
+    //TestTools::showRasters(testRaster, blockRaster);
+}
+
+TEST(VSIImageDriver, readMultisceneResizedSingeChannel) {
+    std::string filePath = TestTools::getFullTestImagePath("vsi", "Zenodo/Abdominal/G1M16_ABD_HE_B6.vsi");
+    std::string testFilePath = TestTools::getFullTestImagePath("vsi", "test-output/G1M16_ABD_HE_B6.vsi-40x_01(1,x=5836,y=11793,w=849,h=607).png");
+    slideio::VSIImageDriver driver;
+    std::shared_ptr<CVSlide> slide = driver.openFile(filePath);
+    ASSERT_TRUE(slide != nullptr);
+    const int numScenes = slide->getNumScenes();
+    ASSERT_EQ(3, numScenes);
+    std::shared_ptr<CVScene> scene = slide->getScene(0);
+    const auto rect = scene->getRect();
+    cv::Rect roi(5836, 11793, 849, 607);
+    cv::Size blockSize(roi.width / 3, roi.height / 3);
+    cv::Mat blockRaster;
+    scene->readResampledBlockChannels(roi, blockSize, {0}, blockRaster);
+    cv::Mat testRaster;
+    TestTools::readPNG(testFilePath, testRaster);
+    cv::resize(testRaster, testRaster, blockSize);
+    cv::extractChannel(testRaster,testRaster,0);
+    double similarity = ImageTools::computeSimilarity2(testRaster, blockRaster);
+    ASSERT_GT(similarity, 0.99);
+    //TestTools::showRasters(testRaster, blockRaster);
+}
+
+TEST(VSIImageDriver, readMultisceneResizedReversedChannels) {
+    std::string filePath = TestTools::getFullTestImagePath("vsi", "Zenodo/Abdominal/G1M16_ABD_HE_B6.vsi");
+    std::string testFilePath = TestTools::getFullTestImagePath("vsi", 
+        "test-output/G1M16_ABD_HE_B6.vsi-40x_01(1,x=5836,y=11793,w=849,h=607).png");
+    slideio::VSIImageDriver driver;
+    std::shared_ptr<CVSlide> slide = driver.openFile(filePath);
+    ASSERT_TRUE(slide != nullptr);
+    const int numScenes = slide->getNumScenes();
+    ASSERT_EQ(3, numScenes);
+    std::shared_ptr<CVScene> scene = slide->getScene(0);
+    const auto rect = scene->getRect();
+    cv::Rect roi(5836, 11793, 849, 607);
+    cv::Size blockSize(roi.width / 3, roi.height / 3);
+    cv::Mat blockRaster;
+    scene->readResampledBlockChannels(roi, blockSize, { 2,1,0 }, blockRaster);
+    cv::Mat testRaster;
+    TestTools::readPNG(testFilePath, testRaster);
+    cv::resize(testRaster, testRaster, blockSize);
+    std::vector<cv::Mat> channels(3);
+    cv::extractChannel(testRaster, channels[2], 0);
+    cv::extractChannel(testRaster, channels[1], 1);
+    cv::extractChannel(testRaster, channels[0], 2);
+    cv::merge(channels, testRaster);
+    double similarity = ImageTools::computeSimilarity2(testRaster, blockRaster);
+    ASSERT_GT(similarity, 0.99);
     //TestTools::showRasters(testRaster, blockRaster);
 }
 
