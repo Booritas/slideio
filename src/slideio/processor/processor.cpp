3d12aae3b784e5d6bacace7dcc0b72d0e0b8327c
Stanislav Melnikov <s.melnikov@smart-reporting.com>
2024-03-27 15:38:01 +0100

    processor

---
+++ src/slideio/processor/processor.cpp
@@ -0,0 +1,10 @@
+// This file is part of slideio project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://slideio.org/license.html.
+#include "processor.hpp"
+
+using namespace slideio;
+
+void Processor::multiResolutionSegmentation(std::shared_ptr<CVScene> scene, int channelIndex, double colorThreshold,
+                     double compactnessThreshold, std::shared_ptr<Storage> storage) { 
+}
---
+++ src/slideio/processor/processor.hpp
@@ -0,0 +1,18 @@
+// This file is part of slideio project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://slideio.org/license.html.
+#pragma once
+#include "slideio/processor/slideio_processor_def.hpp"
+#include "slideio/persistence/storage.hpp"
+
+namespace slideio
+{
+    class SLIDEIO_PROCESSOR_EXPORTS Processor
+    {
+    public:
+        Processor(){}
+        virtual ~Processor() {}
+        void multiResolutionSegmentation(std::shared_ptr<CVScene> scene, int channelIndex, double colorThreshold,
+                     double compactnessThreshold, std::shared_ptr<Storage> storage);
+    };
+};
