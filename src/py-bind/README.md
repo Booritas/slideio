1543722370810b3ffa1257becf155090aa1fadd7
Stanislav Melnikov <stanislav.melnikov@gmail.com>
2024-08-22 21:33:49 +0200

    remove pybind project

--- src/py-bind/README.md
+++
@@ -1,5 +0,0 @@
-Python package for reading of medical images.
-Following formats are supported:
-1. Zeiss czi
-2. Aperio SVS
-3. Generic images (jpeg, tiff, png, etc.)
--- src/py-bind/pyglobals.hpp
+++
@@ -1,9 +0,0 @@
-// This file is part of slideio project.
-// It is subject to the license terms in the LICENSE file found in the top-level directory
-// of this distribution and at http://slideio.com/license.html.
-#pragma once
-#include "pyslide.hpp"
-
-std::shared_ptr<PySlide> pyOpenSlide(const std::string& path, const std::string& driver);
-std::vector<std::string> pyGetDriverIDs();
-void pySetLogLevel(const std::string& level);
