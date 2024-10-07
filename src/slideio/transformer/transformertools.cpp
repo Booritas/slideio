e6d6def0ee64926eaac92b0713e9c8e821ff271f
Stanislav Melnikov <s.melnikov@smart-reporting.com>
2024-10-07 17:01:38 +0200

    update for c++17

--- src/slideio/transformer/transformertools.cpp
+++ src/slideio/transformer/transformertools.cpp
@@ -1,7 +1,6 @@
 // This file is part of slideio project.
 // It is subject to the license terms in the LICENSE file found in the top-level directory
 // of this distribution and at http://slideio.com/license.html.
-#pragma once
 #include "transformertools.hpp"
 #include "transformationex.hpp"
 
