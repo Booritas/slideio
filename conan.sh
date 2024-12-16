dc2bfbbe31d73721b2f4dbbc9efbda9c85bdffec
Stanislav Melnikov <s.melnikov@MacBook-Air.fritz.box>
2024-12-16 19:13:30 +0100

    macos conan profiles and script

--- conan.sh
+++ conan.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
-# Check if the OS is Ubuntu
-if [ "$(lsb_release -is)" = "Ubuntu" ]; then
-    profile=$SLIDEIO_HOME/conan/Linux/ubuntu/linux_release
+set -e
+if [ "$(uname)" = "Linux" ]; then
+    if [ "$(lsb_release -is)" = "Ubuntu" ]; then
+        release_profile=$SLIDEIO_HOME/conan/Linux/ubuntu/linux_release
+        debug_profile=$SLIDEIO_HOME/conan/Linux/ubuntu/linux_debug
+    else
+        echo "Error: No Conan profile for this Linux distribution."
+        exit 1
+    fi
+elif [ "$(uname)" = "Darwin" ]; then
+    if [ "$(uname -m)" = "arm64" ]; then
+        release_profile=$SLIDEIO_HOME/conan/OSX/arm/osx_release
+        debug_profile=$SLIDEIO_HOME/conan/OSX/arm/osx_debug
+    elif [ "$(uname -m)" = "x86_64" ]; then
+        release_profile=$SLIDEIO_HOME/conan/OSX/x86-64/osx_release
+        debug_profile=$SLIDEIO_HOME/conan/OSX/x86-64/osx_debug
+    else
+        echo "Error: No conan profile for this macOS architecture."
+        exit 1
+    fi
+else
+    echo "Error: No conan profile for this operating system."
+    exit 1
 fi
+
 # Function to execute the conan create command
 invoke_conan_create() {
     local folder_path=$1
@@ -20,12 +41,20 @@ invoke_conan_create() {
 # Save the original directory
 original_dir=$(pwd)
 
-invoke_conan_create "recipes/glog/all" "0.6.0"
-invoke_conan_create "recipes/opencv/4.x" "4.10.0"
-invoke_conan_create "recipes/jpegxrcodec/all" "1.0.3"
-invoke_conan_create "recipes/ndpi-libjpeg-turbo/all" "2.1.2"
-invoke_conan_create "recipes/ndpi-libtiff/all" "4.3.0"
-invoke_conan_create "recipes/pole/all" "1.0.4"
+create_conan_recipes() {
+    invoke_conan_create "recipes/glog/all" "0.6.0"
+    invoke_conan_create "recipes/opencv/4.x" "4.10.0"
+    invoke_conan_create "recipes/jpegxrcodec/all" "1.0.3"
+    invoke_conan_create "recipes/ndpi-libjpeg-turbo/all" "2.1.2"
+    invoke_conan_create "recipes/ndpi-libtiff/all" "4.3.0"
+    invoke_conan_create "recipes/pole/all" "1.0.4"
+}
+
+# Call the function
+profile=$release_profile
+#create_conan_recipes
+profile=$debug_profile
+create_conan_recipes
 
 # Change back to the original directory
 cd "$original_dir" || exit
