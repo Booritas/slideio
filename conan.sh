46aba520c65d25ca9ffa3a2b542a0b59f32f1202
Stanislav Melnikov <stanislav.melnikov@gmail.com>
2026-01-10 16:59:30 +0100

    add dcmtk to deps script

--- conan.sh
+++ conan.sh
@@ -112,6 +112,7 @@ create_conan_recipes() {
     invoke_conan_create_slideio "recipes/ndpi-libjpeg-turbo/all" "2.1.2"
     invoke_conan_create_slideio "recipes/ndpi-libtiff/all" "4.3.0"
     invoke_conan_create_slideio "recipes/pole/all" "1.0.4"
+    invoke_conan_create_slideio "recipes/dcmtk/all" "3.6.8"
 }
 
 # Call the function
